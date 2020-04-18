71e6dd8769c14fdd1d3a6955dec559027081cf06
islamsaied <62719593+islamsaied@users.noreply.github.com>
2020-04-18 20:28:25 +0200

    adding repos for comment and post entities

---
+++ Backend/Linkedout.Infrastructure/Repository/CommentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Linkedout.Domain.Entities;
+using Linkedout.Domain.Interfaces.Repository;
+using Linkedout.Infrastructure.Repository.SqlServerContext;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linkedout.Infrastructure.Repository
+{
+    public class CommentRepository : SqlServerRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(IUnitOfWork unitOfWork,
+          LinkedoutEntities context) : base(unitOfWork, context) { }
+    }
+}
---
+++ Backend/Linkedout.Infrastructure/Repository/PostRepository.cs
@@ -0,0 +1,17 @@
+﻿using Linkedout.Domain.Entities;
+using Linkedout.Domain.Interfaces.Repository;
+using Linkedout.Infrastructure.Repository.SqlServerContext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace Linkedout.Infrastructure.Repository
+{
+    public class PostRepository : SqlServerRepository<Post>, IPostRepository
+    {
+
+        public PostRepository(IUnitOfWork unitOfWork,
+           LinkedoutEntities context) : base(unitOfWork, context) { }
+    }
+}
